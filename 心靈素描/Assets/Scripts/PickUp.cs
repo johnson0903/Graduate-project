e25c8ab2ee0786479ba868ae8c301960d4449d7d
Johnson Lee <Johnson Lee>
2016-05-06 13:57:21 +0800

    對話框加入圖片、修改對話系統

--- 心靈素描/Assets/Scripts/PickUp.cs
+++ 心靈素描/Assets/Scripts/PickUp.cs
@@ -2,13 +2,15 @@
 using System.Collections;
 
 public class PickUp : MonoBehaviour {
-
+	public string dialog;
 	private DialogManager dMan;
+	private SpriteRenderer spriteRenderer;
 
 	void Start(){
 		dMan = FindObjectOfType<DialogManager> ();
+		spriteRenderer = GetComponent<SpriteRenderer> ();
 	}
 	public void ShowDialog(){
-		dMan.ShowBox("這是草莓嗎?為什麼這裡會有草莓....看上去好像很好吃");
+		dMan.ShowBox(dialog, spriteRenderer.sprite);
 	}
 }
