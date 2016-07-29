45fb6be18969c4b092d2e73e9c628f421bb23f4a
Kaku Kuo <Kaku Kuo>
2016-07-29 21:12:33 +0800

    先推再說

--- 心靈素描/Assets/Scripts/OpenDoor.cs
+++ 心靈素描/Assets/Scripts/OpenDoor.cs
@@ -3,15 +3,11 @@ using System.Collections;
 
 public class OpenDoor : MonoBehaviour {
 	
-	public Texture2D openDoorCursor;
-
 	private Transform player;
-	private bool isLocked = true;
 	private static bool isGameStart;
-	private bool isPlayerInRange;
 
 	void Start () {
-		Cursor.SetCursor (null, Vector2.zero, CursorMode.Auto);
+
 		player = FindObjectOfType<PlayerController> ().gameObject.transform;
 		if (isGameStart) {	
 			player.position = new Vector3 (this.transform.position.x, player.position.y, player.position.z);
@@ -20,13 +16,5 @@ public class OpenDoor : MonoBehaviour {
 		}
 	}
 
-	void OnMouseEnter()
-	{
-		Cursor.SetCursor (openDoorCursor, Vector2.zero, CursorMode.Auto);
-	}
-
-	void OnMouseExit(){
-		Cursor.SetCursor (null, Vector2.zero, CursorMode.Auto);
-	}
 
 }
