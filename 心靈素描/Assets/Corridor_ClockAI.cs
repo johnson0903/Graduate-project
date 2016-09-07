71a7f447a6674ec0021b13683f30cb1020215441
Kaku <a87000012@gmail.com>
2016-09-07 15:20:39 +0800

    push myself

---
+++ 心靈素描/Assets/Corridor_ClockAI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Corridor_ClockAI : MonoBehaviour {
+
+	private static int hour;
+
+	// Use this for initialization
+	void Start () {
+		hour = 8;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.FindChild ("Hour").transform.rotation = Quaternion.Euler(0, 0, hour * (-30));
+	}
+
+	public void AdjusyTime(int range){
+		hour += range;
+		if (hour >= 12)
+			hour -= 12;
+	}
+
+	public int Hour {
+		get{ return hour; }
+	}
+}
---
+++ 心靈素描/Assets/SymmetryPuzzle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SymmetryPuzzle : MonoBehaviour {
+
+	public GameObject clock;
+	public GameObject umBrellaGirl_Left;
+	public GameObject umBrellaGirl_Right;
+	public Sprite umbrellaGirlWithBlood;
+
+	private static bool isSymmetryPuzzleOver;
+
+	// Use this for initialization
+	void Start () {
+		if (isSymmetryPuzzleOver) {
+			umBrellaGirl_Left.transform.FindChild("GirlWithUmbrella").GetComponent<SpriteRenderer> ().sprite = umbrellaGirlWithBlood;
+			umBrellaGirl_Right.transform.FindChild("GirlWithNoUmbrella").GetComponent<SpriteRenderer> ().sprite = umbrellaGirlWithBlood;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (umBrellaGirl_Left.transform.FindChild("GirlWithUmbrella").GetComponent<SpriteRenderer> ().sprite == umbrellaGirlWithBlood &&
+			umBrellaGirl_Right.transform.FindChild("GirlWithNoUmbrella").GetComponent<SpriteRenderer> ().sprite == umbrellaGirlWithBlood &&
+		    (clock.GetComponent<Corridor_ClockAI> ().Hour == 0 || clock.GetComponent<Corridor_ClockAI> ().Hour == 6)) {
+			isSymmetryPuzzleOver = true;
+			Debug.Log ("66666");
+		}
+	}
+}
