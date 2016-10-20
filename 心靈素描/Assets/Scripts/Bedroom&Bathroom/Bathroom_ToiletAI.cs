0e100fa0066e2af58a557aedb92b9125d215cb0c
Johnson <johnson50701@gmail.com>
2016-10-20 08:27:08 +0800

    解決問題

--- 心靈素描/Assets/Scripts/Bedroom&Bathroom/Bathroom_ToiletAI.cs
+++ 心靈素描/Assets/Scripts/Bedroom&Bathroom/Bathroom_ToiletAI.cs
@@ -10,10 +10,6 @@ public class Bathroom_ToiletAI : MonoBehaviour {
 
 	private GameObject player;
 	private DialogHolder dialogHolder;
-	public GameObject toilet;
-	public Sprite openToilet;
-	public Sprite closeToilet;
-	private bool isToiletOpen;
 
 	// Use this for initialization
 	void Start()
@@ -21,30 +17,13 @@ public class Bathroom_ToiletAI : MonoBehaviour {
 		player = FindObjectOfType<PlayerController>().gameObject;
 		dialogHolder = this.GetComponent<DialogHolder>();
 		this.GetComponent<DialogHolder>().DialogOverEvent += OnDialogOver;
+		this.GetComponent<DialogHolder>().EventDialogEvent += OnEventDialogOccur;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		dialogHolder.Dialogs = new List<Dialog> { dialogHolder.TalkDialog ("用陶瓷做的 純白色的馬桶"),
-			dialogHolder.TalkDialog ("馬桶蓋子是蓋上的"),
-			dialogHolder.AskDialog ("將蓋子打開", "不理他", new List<Dialog> {
-				dialogHolder.TalkDialog ("調查其他地方吧")
-			}),
-			dialogHolder.EventDialog("嘎拉－", true, 0, .2f)
-		};
-	}
-
-	void OnDialogOver(object sender, EventArgs e)
-	{
-		isToiletOpen = !isToiletOpen;
-		if (!isToiletOpen)
-			toilet.GetComponent<SpriteRenderer>().sprite = openToilet;
-		else
-			toilet.GetComponent<SpriteRenderer>().sprite = openToilet;
-			
-=======
-		if (this.GetComponent<SpriteRenderer> ().sprite == closedToilet) {
+		if (this.GetComponent<SpriteRenderer>().sprite == closedToilet)
 			dialogHolder.Dialogs = new List<Dialog> { dialogHolder.TalkDialog ("用陶瓷做的 純白色的馬桶"),
 				dialogHolder.TalkDialog ("馬桶蓋子是蓋上的"),
 				dialogHolder.AskDialog ("將蓋子打開", "不理他", new List<Dialog> {
@@ -53,7 +32,7 @@ public class Bathroom_ToiletAI : MonoBehaviour {
 				dialogHolder.EventDialog ("嘎拉－", true, 0, .5f),
 				dialogHolder.TalkDialog ("沒發現什麼特別的東西...")
 			};
-		} else
+		else
 			dialogHolder.Dialogs = new List<Dialog> { dialogHolder.TalkDialog ("用陶瓷做的 純白色的馬桶"),
 				dialogHolder.TalkDialog ("馬桶蓋子是打開的"),
 				dialogHolder.AskDialog ("將蓋子蓋上", "不理他", new List<Dialog> {
@@ -65,8 +44,8 @@ public class Bathroom_ToiletAI : MonoBehaviour {
 	}
 
 	void OnDialogOver(object sender, EventArgs e)
-	{	
->>>>>>> 5958b7edb1a823e5d68c073da0572aecee400eb1
+	{
+		
 	}
 
 	void OnEventDialogOccur(object sender, EventArgs e)
